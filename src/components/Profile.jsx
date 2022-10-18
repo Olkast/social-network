4c4c50d597645d3de939957035509624382e1081
Alex Vagulis ( home ) <vagulisal500@gmail.com>
2022-10-18 14:41:23 +0300

    init

---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://dubaitrippackages.files.wordpress.com/2017/11/2-imgdinosaurs_base.jpg" alt=""/>
+            </div>
+            <div>ava + description</div>
+            <div>My post</div>
+            <div>New post</div>
+            <div>post 1</div>
+            <div>post 2</div>
+        </div>
+    )
+}
+
+export default Profile;
